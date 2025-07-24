33760d963ca538c5d090878f40977acf5962513c
mspa4681 <mspa4681@uni.sydney.edu.au>
2025-07-24 10:02:54 +1000

    Added changes to gasvel_statistics

--- gasvel_statistics.py
+++ gasvel_statistics.py
@@ -28,12 +28,41 @@ ifs_path = "/import/hortus1/sami/dr3_ingestion_v8/data/sami/dr3/ifs"
 # Location of SAMI AGN Summary Catalogue
 AGN_Summary_path = "SAMI_AGN_matches.fits"
 
-AGN_Summary_table = fits.open(AGN_summary_path)[1].data
+AGN_Summary_table = fits.open(AGN_Summary_path)[1].data
 all_CATIDS = AGN_Summary_table['CATID']
 
-def get_vel_statistics_table(catids=all_CATIDs, ifs_path=ifs_path, SN_lim=5.0):
+def get_vel_statistics_table(catids=all_CATIDs, ifs_path=ifs_path, snlim=5.0):
+    """
+    Create a table containing velocity dispersion statistics for each given CATID.
+    """
     # setup table
-    vel_statistics_table = Table(
+    vel_statistics_table = Table(names=['CATID', 'MEDIAN_VEL_DISP', 'VEL_DISP_SEM'])
+
+    # get the gassig cube for each CATID
+    for catid in catids:
+        # first get ha cube for S/N mask
+        haflux_file = cube_fctns.get_specific_cube_file(catid, 'haflux', ifs_path=ifs_path)
+        haflux = fits.getdata(haflux_file, ext=0)[0,:,:]
+        haerr =  fits.getdata(haflux_file, extname='HALPHA_ERR')[0,:,:]
+        hasn = haflux/haerr
+        ha_snflag = np.where((hasn > snlim),0,1)
+
+
+        gassig_file = cube_fctns.get_specific_cube_file(catid, 'gassig', ifs_path=ifs_path)
+
+        gassig = fits.getdata(gassig_file, ext=0)[0,:,:]
+        gassig_err = fits.getdata(gassig_file, extname=1)[0,:,:]
+
+        gassig_masked = np.ma.masked_array(gassig,(ha_snflag>0))
+        gassig_err_masked = np.ma.masked_array(gassig_err,(ha_snflag>0))
+
+
+        # get the median velocity dispersion (and SEM)
+
+        median_vel_disp = np.ma.median(gassig_masked)
+        vel_disp_sem = stats.sem(gassig_masked.compressed())
     
+        # add to table
+        vel_statistics_table.add_row([catid, median_vel_disp, vel_disp_sem])
     
     
