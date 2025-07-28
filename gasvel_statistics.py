23cd14813bb534126519e6a34948fbe997b2f07c
mspa4681 <mspa4681@uni.sydney.edu.au>
2025-07-28 11:32:10 +1000

    Added 5 arcsec centroid statistics for get_gassig_statistics_table()

--- gasvel_statistics.py
+++ gasvel_statistics.py
@@ -32,30 +32,31 @@ AGN_Summary_table = fits.open(AGN_Summary_path)[1].data
 all_CATIDs = AGN_Summary_table['CATID']
 
 
-def get_gassig_statistics_table(catids=all_CATIDs, save_filepath=os.path.join('shared_catalogues','gassig_statistics.fits'), ifs_path=ifs_path, snlim=10.0, save_file = True):
+def get_gassig_statistics_table(catids=all_CATIDs, save_filepath=os.path.join('shared_catalogues','gassig_statistics.fits'), 
+                                ifs_path=ifs_path, snlim=10.0, save_file = True, centroid_radius=3.0):
     """
     Create a table containing velocity dispersion statistics for each given CATID.
     """
     # setup table
-    vel_statistics_table = Table(names=['CATID', 'MEDIAN_VEL_DISP', 'VEL_DISP_SEM'], dtype=[int, float, float])
+    vel_statistics_table = Table(names=['CATID', 'MEDIAN_VEL_DISP', 'VEL_DISP_SEM', '5ARCSEC_MEDIAN_VEL_DISP', '5ARCSEC_VEL_DISP_SEM'], dtype=[int, float, float])
 
     # get the gassig cube for each CATID
     for catid in catids:
         # first get ha cube for S/N mask
         haflux_file = cube_fctns.get_specific_cube_file(catid, 'haflux', ifs_path=ifs_path)
         try:
-            haflux = fits.getdata(haflux_file, ext=0)[0,:,:]
+            haflux = fits.getdata(haflux_file, ext=0)
         except Exception as e:
             continue
-        haerr =  fits.getdata(haflux_file, extname='HALPHA_ERR')[0,:,:]
+        haerr =  fits.getdata(haflux_file, extname='HALPHA_ERR')
         hasn = haflux/haerr
         ha_snflag = np.where((hasn > snlim),0,1)
 
 
         gassig_file = cube_fctns.get_specific_cube_file(catid, 'gassig', ifs_path=ifs_path)
 
-        gassig = fits.getdata(gassig_file, ext=0)[0,:,:]
-        gassig_err = fits.getdata(gassig_file, extname='VDISP_ERR')[0,:,:]
+        gassig = fits.getdata(gassig_file, ext=0)
+        gassig_err = fits.getdata(gassig_file, extname='VDISP_ERR')
 
         gassig_masked = np.ma.masked_array(gassig,(ha_snflag>0))
         gassig_err_masked = np.ma.masked_array(gassig_err,(ha_snflag>0))
@@ -64,9 +65,34 @@ def get_gassig_statistics_table(catids=all_CATIDs, save_filepath=os.path.join('s
         # get the median velocity dispersion (and SEM)
         median_vel_disp = np.ma.median(gassig_masked)
         vel_disp_sem = stats.sem(gassig_masked.compressed())
-    
+
+
+
+        # Now, get the centre of the cube (centroid based on flux)
+        # first need stelflux cube
+        stelvel_file = cube_fctns.get_specific_cube_file(catid, 'stelvel', ifs_path=ifs_path)
+        #stelvel = fits.getdata(stelvel_file, ext=0)
+        stelflux = fits.getdata(stelvel_file, extname='FLUX')
+
+        stelflux_masked = np.ma.masked_array(stelflux, (ha_snflag>0))
+
+        # now get the centroid
+        centroid_position = cube_fctns.get_centroid_position(stelflux_masked, centroid_radius=centroid_radius)
+
+        # get the median velocity dispersion within 5 spaxels of the centroid
+        if centroid_position is not None:
+            reduced_5arcsec_gassig_masked = cube_fctns.apply_nan_circle_mask(gassig_masked, centroid_position, radius=5)
+            median_5arcsec_vel_disp = np.ma.median(reduced_5arcsec_gassig_masked)
+            sem_5arcsec_vel_disp = stats.sem(reduced_5arcsec_gassig_masked.compressed())
+
+        else:
+            median_5arcsec_vel_disp = np.nan
+            sem_5arcsec_vel_disp = np.nan
+
         # add to table
-        vel_statistics_table.add_row([catid, median_vel_disp, vel_disp_sem])
+        vel_statistics_table.add_row([catid, median_vel_disp, vel_disp_sem, median_5arcsec_vel_disp, sem_5arcsec_vel_disp])
+
+
         
         
     if save_file == True:
