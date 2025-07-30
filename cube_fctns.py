Added minimum valid spaxel threshold for gasvel_statistics and fixed masking of cube_fctns.apply_nan_circle_mask(). Also a few minor changes

cube_fctns.py
@@ -106,6 +106,9 @@ def apply_nan_circle_mask(array, radius, center, array_dim=3):
     Returns:
         masked_array: np.ndarray of same shape with NaNs outside the circle
     """
+    # Ensure input is a masked array
+    array = np.ma.masked_array(array)
+    
     if array_dim == 3:
         λ_dim, x_dim, y_dim = array.shape
         
@@ -119,13 +122,18 @@ def apply_nan_circle_mask(array, radius, center, array_dim=3):
     cx, cy = center
     mask2d = ((x - cx)**2 + (y - cy)**2) <= radius**2 
 
+
     # Apply 2D mask
     if array.ndim == 3:
         mask = np.broadcast_to(mask2d, array.shape)
     else:
         mask = mask2d
-
-    masked_array = np.where(mask, array, np.nan)
+    
+    # since the given array is normally a masked array
+    #masked_array = np.ma.where(mask, array, np.nan)
+    masked_array = array.copy()
+    
+    masked_array.mask = masked_array.mask | ~mask
     return masked_array
 
     

gasvel_statistics.py
@@ -32,14 +32,18 @@ AGN_Summary_table = fits.open(AGN_Summary_path)[1].data
 all_CATIDs = AGN_Summary_table['CATID']
 
 
-def get_gassig_statistics_table(catids=all_CATIDs, save_filepath=os.path.join('shared_catalogues','gassig_statistics.fits'), 
-                                ifs_path=ifs_path, snlim=10.0, save_file = True, centroid_radius=3.0):
+def get_gassig_statistics_table(catids=all_CATIDs, save_filepath=os.path.join('shared_catalogues','gassig_statistics.fits'), ifs_path=ifs_path, snlim=10.0, save_file = False, centroid_radius=3.0, min_spaxel_count = 10, cube_plots = False):
     """
     Create a table containing velocity dispersion statistics for each given CATID.
     """
     # setup table
     vel_statistics_table = Table(names=['CATID', 'MEDIAN_VEL_DISP', 'VEL_DISP_SEM', '5SPAXEL_MEDIAN_VEL_DISP', '5SPAXEL_VEL_DISP_SEM'], dtype=[int, float, float, float, float])
+    
+    if cube_plots:
+        pdf = PdfPages(f"test_plots/gassig_cube_testing")
 
+    count =0
+    
     # get the gassig cube for each CATID
     for catid in catids:
         print(catid)
@@ -61,15 +65,28 @@ def get_gassig_statistics_table(catids=all_CATIDs, save_filepath=os.path.join('s
 
         gassig_masked = np.ma.masked_array(gassig,(ha_snflag>0))
         
-        axs[0].imshow(gassig_masked, origin='lower')
-        axs[0].set_title('gassig_masked')
-        
         gassig_err_masked = np.ma.masked_array(gassig_err,(ha_snflag>0))
 
+        # check enough valid spaxels
+        gassig_masked_num_spaxels = np.sum(np.isfinite(gassig_masked))
+        
         # get the median velocity dispersion (and SEM)
-        median_vel_disp = np.ma.median(gassig_masked)
-        vel_disp_sem = stats.sem(gassig_masked.compressed())
+        if gassig_masked_num_spaxels > min_spaxel_count:
+            median_vel_disp = np.ma.median(gassig_masked)
+            vel_disp_sem = stats.sem(gassig_masked.compressed())
+        else:
+            median_vel_disp = np.nan
+            vel_disp_sem = np.nan
+            
+            
+        if cube_plots:
+            fig1 = py.figure(1,constrained_layout=True)
+            fig1.clf()
+            axs = fig1.subplots(1,3)
+            axs = axs.flatten()
 
+            axs[0].imshow(gassig_masked, origin='lower')
+            axs[0].set_title(f'gassig_masked - {gassig_masked_num_spaxels}')
 
 
         # Now, get the centre of the cube (centroid based on flux)
@@ -81,19 +98,32 @@ def get_gassig_statistics_table(catids=all_CATIDs, save_filepath=os.path.join('s
         # now get the centroid
         centroid_position = cube_fctns.get_centroid(stelflux, averaging_radius=centroid_radius, array_dim=2)
         
-        axs[1].imshow(stelflux, origin='lower')
-        axs[1].plot(centroid_position[1], centroid_position[0], '.')
-        axs[1].set_title('stelflux')
-       
+        if cube_plots:
+            axs[1].imshow(stelflux, origin='lower')
+            axs[1].plot(centroid_position[1], centroid_position[0], '.')
+            axs[1].set_title('stelflux')
+            
+            axs[0].plot(centroid_position[1], centroid_position[0], '.')
+            axs[2].plot(centroid_position[1], centroid_position[0], '.')
 
         # get the median velocity dispersion within 5 spaxels of the centroid
-        if centroid_position is not None:
+        if centroid_position is not None:            
             reduced_5spaxel_gassig_masked = cube_fctns.apply_nan_circle_mask(gassig_masked, 5, centroid_position, array_dim = 2)
-            median_5spaxel_vel_disp = np.nanmedian(reduced_5spaxel_gassig_masked)
+            
+            reduced_5spaxel_gassig_masked_num_spaxels = np.sum(np.isfinite(reduced_5spaxel_gassig_masked))
 
-            sem_5spaxel_vel_disp = stats.sem(reduced_5spaxel_gassig_masked, axis=None, nan_policy='omit')
-            axs[2].imshow(reduced_5spaxel_gassig_masked, origin='lower')
-            axs[2].set_title('5spaxel_gassig')
+            
+            if reduced_5spaxel_gassig_masked_num_spaxels > min_spaxel_count:
+                median_5spaxel_vel_disp = np.nanmedian(reduced_5spaxel_gassig_masked)
+                sem_5spaxel_vel_disp = stats.sem(reduced_5spaxel_gassig_masked, axis=None, nan_policy='omit')
+            else:
+                median_5spaxel_vel_disp = np.nan
+                sem_5spaxel_vel_disp = np.nan
+
+
+            if cube_plots:
+                axs[2].imshow(reduced_5spaxel_gassig_masked, origin='lower')
+                axs[2].set_title(f'5spaxel_gassig - {reduced_5spaxel_gassig_masked_num_spaxels}')
 
         else:
             median_5spaxel_vel_disp = np.nan
@@ -101,22 +131,35 @@ def get_gassig_statistics_table(catids=all_CATIDs, save_filepath=os.path.join('s
 
         # add to table
         vel_statistics_table.add_row([catid, median_vel_disp, vel_disp_sem, median_5spaxel_vel_disp, sem_5spaxel_vel_disp])
-
+        
+        if cube_plots:
+            py.draw()
+            py.savefig(pdf, format='pdf')
+            
+            
 
     if save_file == True:
         vel_statistics_table.write(save_filepath, overwrite=True)
-        
+    
+    if cube_plots:
+        pdf.close()
     return vel_statistics_table
     
-fig1 = py.figure(1,constrained_layout=True)
-fig1.clf()
-axs = fig1.subplots(1,3)
-axs = axs.flatten()
-get_gassig_statistics_table()
 
-pdf = PdfPages(f"test_plots/gassig_cube_testing")
-py.draw()
-# pause for input if plotting all the spectra:
-#yn = input('Continue? (y/n):')
-py.savefig(pdf, format='pdf')
-pdf.close()
+get_gassig_statistics_table(cube_plots=False, save_file=True)
+
+#checking high gassig cubes
+'''
+filename = "shared_catalogues/gassig_statistics.fits"
+gassig_statistics = fits.open(filename)
+gassig_table = gassig_statistics[1].data
+
+mask = gassig_table['MEDIAN_VEL_DISP'] > 400
+relevant_CATIDs = gassig_table['CATID'][mask]
+relevant_CATIDs = [np.int64(30615), np.int64(214250), np.int64(273336), np.int64(508132), np.int64(9011900034), np.int64(9011900137), np.int64(9388000003), np.int64(9388000041), np.int64(9403800001), np.int64(9403800117)]
+print(relevant_CATIDs)
+
+get_gassig_statistics_table(relevant_CATIDs, save_filepath=os.path.join('shared_catalogues','test_gassig_statistics.fits'), cube_plots=True)
+'''
+
+

plot_tools.py
@@ -127,7 +127,7 @@ def plot_sov_many_new(catfile, specific_catids= 'All', save_name = 'sov_many.pdf
         isradio_ls = np.zeros(len(tab['IS_RADIOSOURCE']))
 
 
-    if specific_catids != 'All':
+    if type(specific_catids) != str:
         tab = tab[np.isin(tab['CATID'], specific_catids)] # redefine tab to only include specific_catids
         
         #check if any missing catids: